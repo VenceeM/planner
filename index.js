e1d475befbd106e0f474154a5c9d4c6620778b90
officialVencee <officialvencee.manansala@gmail.com>
2021-04-12 11:28:28 +0800

    test

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-})
+
 app.use(cors());
 // app.use(cors({
 //     credentials:true,
