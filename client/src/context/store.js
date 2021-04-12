ba53ece3eec282d73385726618039e6148b949a0
officialVencee <officialvencee.manansala@gmail.com>
2021-04-12 12:29:08 +0800

    test

--- client/src/context/store.js
+++ client/src/context/store.js
@@ -4,8 +4,8 @@ import {createStore,compose,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
-
-const store = createStore(reducers,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+//development ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(reducers,compose(applyMiddleware(thunk)));
 
 function DataProvider({children}) {
     return (
